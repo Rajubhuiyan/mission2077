9f2f076780fa67adba5a6094fe46b97708dd8290
rajubhuiyan <rajubhuiyaann@gmail.com>
2022-01-12 19:08:26 +0600

    m

--- pages/api/missiondata/index.js
+++ pages/api/missiondata/index.js
@@ -1,4 +1,4 @@
-export default function (req, res) {
+export default function tr(req, res) {
 
     const missions = [
         {
@@ -25,3 +25,5 @@ export default function (req, res) {
     res.status(200).json(missions)
 
 }
+
+tr();
